90b4b1bf4959162fd162a43f3504533bbb64d2b5
Glanzmann100 <72605887+Glanzmann100@users.noreply.github.com>
2024-11-06 04:15:48 -0300

    Colocando um sistema de loop
    colocando loop

--- Jogo do Par ou Ímpar.py
+++ Jogo do Par ou Ímpar.py
@@ -1,20 +1,33 @@
 import random
 
 #Solicita a escolha do jogador
-Jogador_Escolha = input("Par ou Impar? ")
-Jogador_Numero = int(input("Escolha um número entre 0 a 10: "))
-Adversário = random.randint(0,10)
+Jogadas = 0
+Jogador = 0
+Adversário = 0 
+Rodadas = 3
 
-#Estabelece a soma e o resultado
-Soma = Jogador_Numero + Adversário
-Resultado = "Par" if Soma % 2 == 0 else "Impar"
+while True:
+    Jogador_Escolha = input("Par ou Impar? ")
+    Jogador_Numero = int(input("Escolha um número entre 0 a 10: "))
+    Adversário = random.randint(0,10)
+    Soma = Jogador_Numero + Adversário
+    Resultado = "Par" if Soma % 2 == 0 else "Impar"
 
-print(f"Número do Adversário: {Adversário}") 
-print(f"Soma: {Soma}")
-print(f"Resultado: {Resultado}")
+    print(f"Número do Adversário: {Adversário}") 
+    print(f"Soma: {Soma} Resultado: {Resultado}")
 
-#Verifica quem ganhou
-if Jogador_Escolha == Resultado: 
-    print("Você venceu!") 
-else: 
-    print("Você perdeu!")
+    if Jogador_Escolha == Resultado:
+        Jogador += 1
+        print("Você venceu")
+    else:
+        Adversário += 1
+        print("Você perdeu!")
+    
+    print(f"Placar: Jogador {Jogador} x {Adversário} Computador")
+
+    if Jogador == Rodadas:
+        print("Parabéns você venceu o jogo")
+        break
+    elif Adversário == Rodadas:
+        print("O Adversário venceu o jogo")
+        break
